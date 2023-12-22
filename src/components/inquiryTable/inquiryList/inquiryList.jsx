81147dfa6f7b0c0ed80083c50a958d7b14fe2f7b
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-12-22 16:17:30 +0900

    refactor : ui 로직과 비즈니스 로직 분리

--- src/components/inquiryTable/inquiryList/inquiryList.jsx
+++ src/components/inquiryTable/inquiryList/inquiryList.jsx
@@ -1,24 +1,14 @@
-import { useState } from "react";
 import { useRQInquiries } from "../../../util/hooks/RQ/useRQInquires";
 import InquiryListDisplay from "./InquiryListDisplay";
 import InquiryDetail from "./inquiryDetail";
 
 const InquiryList = () => {
-  const [selectedInquiryId, setSelectedInquiryId] = useState(null);
-  const { inquiries, deleteInquiry } = useRQInquiries();
-  // 빈 의존성 배열을 전달하여 마운트될 때만 실행되게 합니다.
-  const handleDeleteInquiry = (id) => {
-    deleteInquiry.mutate(id, {
-      onSuccess: () => {
-        // 삭제 후 추가적으로 수행할 작업이 있다면 여기에 작성
-        setSelectedInquiryId(null);
-      },
-      onError: (error) => {
-        console.error("Error deleting inquiry:", error);
-        // 사용자에게 에러 메시지를 표시하는 로직 추가
-      }
-    });
-  };
+  const {
+    inquiries,
+    selectedInquiryId,
+    setSelectedInquiryId,
+    handleDeleteInquiry
+  } = useRQInquiries();
 
   const handleInquirySelect = (id) => {
     setSelectedInquiryId(id);
--- src/util/hooks/RQ/useRQInquires.js
+++ src/util/hooks/RQ/useRQInquires.js
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-
+import { useState } from "react";
 const getInquiries = async () => {
   try {
     const { data } = await axios.get("/inquire");
@@ -13,6 +13,7 @@ const getInquiries = async () => {
 };
 
 export const useRQInquiries = () => {
+  const [selectedInquiryId, setSelectedInquiryId] = useState(null);
   const queryClient = useQueryClient();
 
   const { data: inquiries, ...queryInfo } = useQuery({
@@ -29,5 +30,24 @@ export const useRQInquiries = () => {
     }
   });
 
-  return { inquiries, deleteInquiry, ...queryInfo };
+  const handleDeleteInquiry = (id) => {
+    deleteInquiry.mutate(id, {
+      onSuccess: () => {
+        // 삭제 후 추가적으로 수행할 작업이 있다면 여기에 작성
+        setSelectedInquiryId(null);
+      },
+      onError: (error) => {
+        console.error("Error deleting inquiry:", error);
+        // 사용자에게 에러 메시지를 표시하는 로직 추가
+      }
+    });
+  };
+
+  return {
+    inquiries,
+    selectedInquiryId,
+    setSelectedInquiryId,
+    handleDeleteInquiry,
+    ...queryInfo
+  };
 };
