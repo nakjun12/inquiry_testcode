23ee88ce03f3e0b516cf6eae8014ae10f2ae419a
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-12-28 13:05:43 +0900

    feat : 절대 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path"; // 현재 파일의 디렉토리 경로를 구합니다.
+import { dirname, fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(currentDir, "src") // '@'를 src 폴더의 절대 경로로 설정
+    }
+  }
+});
