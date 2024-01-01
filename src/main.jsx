f8a2a57e2c8d481b63ff5386a88624c726899ad6
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-01 22:22:52 +0900

    feat : msw config 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { USE_MOCK_SERVICE_WORKER } from "./utils/constants/config";
 const queryClient = new QueryClient();
 
 async function enableMocking() {
-  const { worker } = await import("./mocks/browser");
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start({
-    onUnhandledRequest: "bypass"
-  });
+  if (USE_MOCK_SERVICE_WORKER) {
+    const { worker } = await import("./mocks/browser");
+    return worker.start({
+      onUnhandledRequest: "bypass"
+    });
+  }
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 enableMocking().finally(() => {
