55efc8c7c80f48a49cf62f4159b03b5de569de78
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-01-02 14:43:57 +0900

    refactor : react-window 적용
    성능 개선을 위하여 react-window 적용

--- src/pages/inquiry/components/inquiryList/InquiryListDisplay.jsx
+++ src/pages/inquiry/components/inquiryList/InquiryListDisplay.jsx
@@ -1,29 +1,32 @@
-import InquiryListItem from "./inquiryListItem";
-
 /**
- * 문의 목록을 표시하는 컴포넌트입니다.
+ * 문의 목록을 표시하는 컴포넌트입니다. react-window의 FixedSizeList를 사용하여
+ * 문의 목록을 효율적으로 렌더링합니다.
  *
- * @param {Object[]} inquiries - 표시할 문의 목록 배열
- * @param {Function} onSelect - 문의 항목 선택 시 호출되는 함수. 문의 ID를 인자로 받습니다.
+ * @param {Object[]} inquiries - 표시할 문의 목록 배열입니다.
+ * @param {Function} onSelect - 문의 항목을 선택할 때 호출될 함수입니다.
+ *                             각 문의의 ID를 인자로 받습니다.
  */
-const InquiryListDisplay = ({ inquiries, onSelect }) => {
-  // 문의 목록이 비어있는 경우, 안내 메시지를 표시합니다.
+const InquiryList = ({ inquiries, onSelect }) => {
+  // 문의 목록이 비어있는 경우 안내 메시지를 표시합니다.
   if (!inquiries?.length) {
     return <p className="text-center py-4">작성한 문의 내역이 없습니다.</p>;
   }
 
-  // 문의 목록이 있는 경우, 각 문의 항목을 InquiryListItem 컴포넌트로 표시합니다.
+  // itemData는 InquiryRow 컴포넌트에 전달될 데이터입니다.
+  const itemData = { inquiries, onSelect };
+
+  // FixedSizeList 컴포넌트를 사용하여 문의 목록을 렌더링합니다.
   return (
-    <div>
-      {inquiries.map((inquiry) => (
-        <InquiryListItem
-          key={inquiry.id}
-          inquiry={inquiry}
-          onSelect={() => onSelect(inquiry.id)}
-        />
-      ))}
-    </div>
+    <List
+      height={500} // 리스트의 높이
+      itemCount={inquiries.length} // 렌더링할 아이템의 총 개수
+      itemSize={100} // 각 아이템의 높이
+      width={"100%"} // 리스트의 너비
+      itemData={itemData} // InquiryRow 매개변수
+    >
+      {InquiryRow}
+    </List>
   );
 };
 
-export default InquiryListDisplay;
+export default InquiryList;
---
+++ src/pages/inquiry/components/inquiryList/inquiryRow.jsx
@@ -0,0 +1,27 @@
+/**
+ * 문의 목록 내 각 문의 항목을 렌더링하는 컴포넌트입니다.
+ * `react-window`에서 제공하는 `FixedSizeList`에 의해 사용되며,
+ * 각 문의에 대한 정보와 상호작용을 관리합니다.
+ *
+ * @param {object} props - 컴포넌트에 전달되는 props
+ *   @param {number} props.index - 현재 렌더링되는 항목의 인덱스
+ *   @param {object} props.style - `react-window`에 의해 제공되는 스타일링, 각 항목의 위치 지정에 사용됨
+ *   @param {object} props.data - `FixedSizeList`로부터 전달되는 추가 데이터
+ *     @param {object[]} props.data.inquiries - 전체 문의 목록
+ *     @param {function} props.data.onSelect - 문의 항목 선택 시 호출되는 함수, 문의 ID를 인자로 받음
+ */
+const InquiryRow = ({ index, style, data }) => {
+  const { inquiries, onSelect } = data;
+  const inquiry = inquiries[index];
+
+  return (
+    <div style={style} className={`inquiry-row-${index}`}>
+      <InquiryListItem
+        inquiry={inquiry}
+        onSelect={() => onSelect(inquiry.id)}
+      />
+    </div>
+  );
+};
+
+export default InquiryRow;
